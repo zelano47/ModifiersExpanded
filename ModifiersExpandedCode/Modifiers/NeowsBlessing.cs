6ad73eebfbbc66510a4aa29ff0cc6d268259d860
zelano47 <zelano47@gmail.com>
2026-06-23 17:43:22 -0400

    removed placeholder code

--- ModifiersExpandedCode/Modifiers/NeowsBlessing.cs
+++ ModifiersExpandedCode/Modifiers/NeowsBlessing.cs
@@ -9,19 +9,5 @@ namespace ModifiersExpanded.ModifiersExpandedCode.Modifiers;
 
 public class NeowsBlessing : ModifierModel
 {
-    public override Func<Task> GenerateNeowOption(EventModel eventModel)
-    {
-        return () =>
-        {
-            ArgumentNullException.ThrowIfNull(eventModel);
-            return OfferNeowRelics(eventModel.Owner);
-        };
-    }
-
-    public async Task OfferNeowRelics(Player player)
-    {
-        await Task.FromResult(false);
-    }
-
     protected override string IconPath => "neows_blessing.png".ModifierImagePath();
 }
