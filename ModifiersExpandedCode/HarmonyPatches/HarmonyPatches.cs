e0876308220070541402d8a0a27e7f50fbdd10c2
zelano47 <zelano47@gmail.com>
2026-07-05 12:36:30 -0400

    Revert "added localization for Last run button"
    This reverts commit ac0634f80c62c6be186dd48686b68e0aaa92ecbf.

--- ModifiersExpandedCode/HarmonyPatches/HarmonyPatches.cs
+++ ModifiersExpandedCode/HarmonyPatches/HarmonyPatches.cs
@@ -10,7 +10,6 @@ using MegaCrit.Sts2.Core.Events;
 using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.HoverTips;
-using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Enchantments;
 using MegaCrit.Sts2.Core.Models.Events;
@@ -395,12 +394,7 @@ public class HarmonyPatches
             // ready) and set the label to "RANDOMIZE". Override it.
             ((Node)(object)lastRunButton)
                 .GetNode<MegaRichTextLabel>("Label")
-                .SetTextAutoSize(
-                    new LocString(
-                        "main_menu_ui",
-                        "CUSTOM_RUN_SCREEN.LAST_RUN_BUTTON"
-                    ).GetFormattedText()
-                );
+                .SetTextAutoSize("Last Run");
 
             // Wire up the click handler.
             ((GodotObject)(object)lastRunButton).Connect(
